b990501d5e23ad4d13be5b4b43b9fe628391c25f
Samuel <samuelperron@hotmail.fr>
2020-08-30 00:50:44 -0400

    T-4540 - Ingredient serializers

--- api/ingredient/models.py
+++ api/ingredient/models.py
@@ -4,15 +4,35 @@ from django.db import models
 class IngredientCategory(models.Model):
     name = models.CharField(max_length=25, null=True)
 
+    def __str__(self):
+        return name
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
 
 class IngredientSubCategory(models.Model):
     name = models.CharField(max_length=50, null=True)
     parent_category = models.ForeignKey(IngredientCategory, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return f'{parent_category.name} - {name}'
+
+    class Meta:
+        verbose_name = 'sub-category'
+        verbose_name_plural = 'sub-categories'
+
 
 class IngredientBrand(models.Model):
     name = models.CharField(max_length=100, null=True)
     image = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return name
+
+    class Meta:
+        verbose_name = 'brand'
 
 
 class Ingredient(models.Model):
@@ -20,6 +40,9 @@ class Ingredient(models.Model):
     image = models.CharField(max_length=255, blank=True)
     category = models.ForeignKey(IngredientSubCategory, on_delete=models.CASCADE, null=True)
     brand = models.ForeignKey(IngredientBrand, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return name
 
 
 class IngredientServing(models.Model):
--- api/ingredient/serializers.py
+++ api/ingredient/serializers.py
@@ -3,31 +3,38 @@ from rest_framework import serializers
 from .models import Ingredient, IngredientCategory, IngredientSubCategory, IngredientServing, IngredientBrand
 
 
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingredient
-        fields = ['pk']
-
-
 class IngredientCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = IngredientCategory
-        fields = ['pk']
+        fields = '__all__'
 
 
 class IngredientSubCategorySerializer(serializers.ModelSerializer):
+    parent_category = IngredientCategorySerializer()
+
     class Meta:
         model = IngredientSubCategory
-        fields = ['pk']
-
-
-class IngredientServingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IngredientServing
-        fields = ['pk']
+        fields = '__all__'
 
 
 class IngredientBrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = IngredientBrand
-        fields = ['pk']
+        fields = '__all__'
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    category = IngredientSubCategorySerializer()
+    brand = IngredientBrandSerializer()
+
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+
+class IngredientServingSerializer(serializers.ModelSerializer):
+    ingredient = IngredientSerializer()
+
+    class Meta:
+        model = IngredientServing
+        fields = '__all__'
