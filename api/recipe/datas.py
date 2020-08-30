e831142aba3e68088ee967fe71e64c57358b3818
Samuel <samuelperron@hotmail.fr>
2020-08-30 11:56:50 -0400

    T-4660 - Recipe categories

---
+++ api/recipe/datas.py
@@ -0,0 +1,85 @@
+CATEGORIES = {
+    'Appetizers and Snacks': [
+        'Bruschetta',
+        'Canapes and Crostini',
+        'Cheese appetizers',
+        'Chicken wings',
+        'Crackers',
+        'Deviled eggs',
+        'Dips and spread',
+        'Kids snacks',
+        'Party Mix',
+        'Snacks',
+        'Fruit appetizers',
+        'Other appetizers'
+    ],
+    'Breads': [
+        'Bagels',
+        'Banana bread',
+        'Challah',
+        'Egg bread',
+        'Flat bread',
+        'Pizza dough',
+        'Pretzels',
+        'Rolls & Buns',
+        'White bread',
+        'Whole grain bread',
+        'Pastries',
+        'Cornbread',
+        'Other breads'
+    ],
+    'Desserts': [
+        'Cakes & Cupcakes',
+        'Candy',
+        'Caramel desserts',
+        'Cookies',
+        'Custards, Puddings & Mousses',
+        'Pies',
+        'Frozen desserts',
+        'Other desserts'
+    ],
+    'Drinks': [
+        'Cider',
+        'Cocktails & Mocktails',
+        'Coffee drinks',
+        'Eggnog',
+        'Hot Chocolate',
+        'Juice',
+        'Lemonade',
+        'Liqueurs',
+        'Smoothies',
+        'Tea drinks'
+    ],
+    'Side dish': [
+        'Vegetable side dishes',
+        'Potato side dishes',
+        'Rice, Quinoa & Couscous',
+        'Beans side dishes',
+        'Sauces & Condiments',
+        'Soup',
+        'Salads'
+    ],
+    'Breakfast & Brunch': [
+        'Crêpes & Pancakes',
+        'Casseroles',
+        'French toasts',
+        'Waffles',
+        'Cereals',
+        'Eggs & Omelettes',
+        'Breakfast drinks'
+    ],
+    'Lunch': [
+        'Bowls',
+        'Sandwiches & Wraps',
+        'Soup',
+        'Salad',
+        'School lunches'
+    ],
+    'Dinner': [
+        'Week-night dinner',
+        'Meat & Poultry',
+        'Pasta & Noodles',
+        'Seafood',
+        'BBQ & Grilling'
+    ]
+}
