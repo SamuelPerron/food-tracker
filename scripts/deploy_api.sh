c11d7bb027a50494a1e9315d13c21ac27fcdf41b
Samuel <samuelperron@hotmail.fr>
2020-09-18 18:54:39 -0400

    T-4280 - Deploy script API

---
+++ scripts/deploy_api.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pull latest branch from git
+ssh samperron@ssh-samperron.alwaysdata.net << EOF
+cd ./www/food-tracker/
+git fetch
+git checkout $1
+git reset --hard HEAD
+EOF
+
+# Restart server
+curl --basic --user cd61d7460e174eafaa22b203e23329ac: --data '' --request POST https://api.alwaysdata.com/v1/site/614831/restart/
